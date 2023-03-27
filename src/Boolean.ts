1111513ee9ddfdbdd14c5e80f09060303fa0c168
Jesse Kelly <kellyjesse881@gmail.com>
2023-03-27 12:47:06 +0200

    feat(boolean): added mixed cases to parseFuzzy

--- src/Boolean.ts
+++ src/Boolean.ts
@@ -4,41 +4,22 @@ import * as Str from "@effect/data/String";
 import * as PR from "@effect/schema/ParseResult"
 import { pipe } from "@effect/data/Function";
 
-const truthyValues = [
-  "1",
-  "true",
-  "True",
-  "TRUE",
-  "yes",
-  "Yes",
-  "YES",
-  "on",
-  "On",
-  "ON",
-];
+const truthyValues = ["1", "true", "yes", "on", "ok"];
+const falsyValues = ["0", "false", "no", "off"];
 
-const falsyValues = [
-  "0",
-  "false",
-  "False",
-  "FALSE",
-  "no",
-  "No",
-  "NO",
-  "off",
-  "Off",
-  "OFF",
-];
-
-export const booleanFromString = <I>(self: S.Schema<I, string>): S.Schema<I, boolean> => {
+/*
+ * Parses a boolean from a string using a predefined set of truthy and falsy values. E.g. "yes", "no", "on", "off", "1", "0"
+ * @since 1.0.0 
+ */
+export const parseFuzzy = <I>(self: S.Schema<I, string>): S.Schema<I, boolean> => {
   const schema: S.Schema<I, boolean> = S.transformResult(
     self,
     S.boolean,
     (s) => {
-      if (A.contains(Str.Equivalence)(truthyValues, s)) {
+      if (A.contains(Str.Equivalence)(truthyValues, s.toLowerCase())) {
         return PR.success(true);
       }
-      if (A.contains(Str.Equivalence)(falsyValues, s)) {
+      if (A.contains(Str.Equivalence)(falsyValues, s.toLowerCase())) {
         return PR.success(false);
       }
 
--- test/Boolean.ts
+++ test/Boolean.ts
@@ -4,15 +4,16 @@ import * as _ from "../src/Boolean";
 
 describe("Boolean", () => {
     it("booleanFromString", () => {
-        const parse = S.parse(_.booleanFromString(S.string))
+        const parse = S.parse(_.parseFuzzy(S.string))
 
         expect(parse("true")).toBe(true)
-        expect(parse("yes")).toBe(true)
+        expect(parse("Yes")).toBe(true)
         expect(parse("1")).toBe(true)
         expect(parse("on")).toBe(true)
+        expect(parse("OK")).toBe(true)
 
         expect(parse("false")).toBe(false)
-        expect(parse("no")).toBe(false)
+        expect(parse("No")).toBe(false)
         expect(parse("0")).toBe(false)
         expect(parse("false")).toBe(false)
     })
