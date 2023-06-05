61a595f700629093563c31a99373cfd40eee6040
Jesse Kelly <kellyjesse881@gmail.com>
2023-06-05 00:23:32 -0600

    feat: added Person module

--- src/Internet.ts
+++ src/Internet.ts
@@ -27,3 +27,31 @@ export const Port = pipe(S.number, port(), S.brand("Port"));
  * @since 1.0.0
  */
 export type Port = S.To<typeof Port>
+
+
+
+/**
+ * @category datatypes
+ * @since 1.0.0
+ */
+export const HttpMethod = S.literal("GET", "POST", "PUT", "DELETE", "PATCH")
+
+/**
+ * @category brands
+ * @since 1.0.0
+ */
+export type HttpMethod = S.To<typeof HttpMethod>
+
+
+
+/**
+ * @category datatypes
+ * @since 1.0.0
+ */
+export const Protocol = S.literal("http", "https")
+
+/**
+ * @category brands
+ * @since 1.0.0
+ */
+export type Protocol = S.To<typeof Protocol>
---
+++ src/Person.ts
@@ -0,0 +1,14 @@
+import * as S from "@effect/schema/Schema";
+
+
+/**
+ * @category datatypes
+ * @since 1.0.0
+ */
+export const Sex = S.literal("male", "female")
+
+/**
+ * @category brands
+ * @since 1.0.0
+ */
+export type Sex = S.To<typeof Sex>
